f6f44cde225453255ee1b24b2de4013e209cc885
Salomon Elguera <106698702+Salo1989@users.noreply.github.com>
2022-07-12 21:13:41 +0300

    Add files via upload

---
+++ sopranos question.py
@@ -0,0 +1,10 @@
+print("Do you like watching the Sopranos?")
+user_input=input("Type yes or no:")
+ans_one="yes"
+ans_two="no"
+if user_input==ans_one:
+    print("You have great taste in Television series")
+    print("You should watch Breaking Bad and The Wire")
+
+else:
+    print(" Go die, you have no idea what good television is")
